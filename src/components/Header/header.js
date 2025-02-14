62a6faf4a82c003ed45c51ff30d109fdfea99a75
Sainath0009 <sainathmedpalli@.com>
2025-02-14 12:23:50 +0530

    header search is ready

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,9 +1,33 @@
-import React from 'react'
-import '../Header/header.css';
+import React from "react";
+import "../Header/header.css";
+import logo from "../../assets/logo.svg";
+import SearchIcon from "@mui/icons-material/Search";
+
 const Header = () => {
   return (
-    <div>this is header </div>
-  )
-}
+    <>
+      <header>
+        <div className="container-fluid">
+          <div className="row align-items-center">
+            <div className="col-sm-2">
+              <img src={logo} alt="Logo" />
+            </div>
 
-export default Header
+            {/* Search Section */}
+            <div className="col-sm-5">
+              <div className="headerSearch d-flex align-items-center">
+                <div className="selectDrop">All Categories</div>
+                <div className="search">
+                  <input type="text" placeholder="Search..." />
+                  <SearchIcon className="searchIcon" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
